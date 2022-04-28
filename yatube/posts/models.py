f91f50d93e708297cb030d5e64d6ddd4ee03d1d7
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-04-28 22:05:48 +1000

    Добавлена база и админка

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    ) 
