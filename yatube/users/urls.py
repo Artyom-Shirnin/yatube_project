953988b79c3eb2f5548ced66e9f30b7a7ed94869
Артём Ширнин <artyom.shirnin@yandex.ru>
2022-05-10 20:00:44 +1000

    Изменения после майских

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,14 +1,20 @@
-from django.contrib.auth.views import LogoutView 
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from . import views
 
 app_name = 'users'
 
 urlpatterns = [
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path(
       'logout/',
       LogoutView.as_view(template_name='users/logged_out.html'),
       name='logout'
     ),
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path(
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
 ] 
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -88,3 +88,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+# LOGOUT_REDIRECT_URL = 'posts:index'
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
